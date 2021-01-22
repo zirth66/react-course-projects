645b215c895b1a8867a7b97aae4115aca648ad00
zirth <zirth66@gmail.com>
2021-01-22 15:04:15 +0100

    9. Setting up Babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,14 @@
+"use strict";
+
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-//const template = <p>This is JSX from app.js</p>;
-var template = /*#__PURE__*/React.createElement("h1", {
-    id: "someid"
-  }, "This is JSX from app.js");
-const appRoot = document.getElementById("app");
+var template = React.createElement(
+  "h1",
+  null,
+  "WTF"
+);
+var appRoot = document.getElementById("app");
 
 // Render this template in this element
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log("App.js is running");
+
+// JSX - JavaScript XML
+const template = <h1>WTF</h1>;
+const appRoot = document.getElementById("app");
+
+// Render this template in this element
+ReactDOM.render(template, appRoot);
