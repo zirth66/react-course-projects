cee768d1b3ca7bb1268e74900ef4353bab6be19f
zirth <zirth66@gmail.com>
2021-01-22 15:40:42 +0100

    10. Exploring JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,11 +4,55 @@ console.log("App.js is running");
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "WTF"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "WTF"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is someone info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Zirth Starfighter"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: Eternal"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Cyberspace"
+    )
+);
+
 var appRoot = document.getElementById("app");
 
 // Render this template in this element
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,27 @@
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-const template = <h1>WTF</h1>;
+const template = (
+    <div>
+        <h1>WTF</h1> 
+        <p>This is someone info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const templateTwo = (
+    <div>
+        <h1>Zirth Starfighter</h1>
+            <p>Age: Eternal</p>
+            <p>Location: Cyberspace</p>
+    </div>
+);
+
 const appRoot = document.getElementById("app");
 
 // Render this template in this element
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
+
