ef5b27f88a68cdad81393d326aa14c0867db58be
zirth <zirth66@gmail.com>
2021-01-23 10:04:30 +0100

    14. ES6 Aside: Arrow Functions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,78 +1,11 @@
 "use strict";
 
-console.log("App.js is running");
-
-var app = {
-    title: "Indecision App",
-    subtitle: "When you cant decide",
-    options: ["One", "Two"]
-
-    // JSX - JavaScript XML
-};var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? "Here are your options" : "No options"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "Item one"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item two"
-        )
-    )
-);
-var user = {
-    name: "Daniel",
-    age: 40,
-    location: "Sweden"
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(" ")[0];
 };
-var getLocation = function getLocation(location) {
-    return location ? React.createElement(
-        "p",
-        null,
-        "Location: ",
-        location
-    ) : "";
+console.log(getFirstName("Daniel Persson"));
+
+var getLastName = function getLastName(fullName) {
+    return fullName.split(" ")[1];
 };
-
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : "Anonymous"
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        "p",
-        null,
-        "Age: ",
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-var appRoot = document.getElementById("app");
-
-// Render this template in this element
-ReactDOM.render(template, appRoot);
+console.log(getLastName("Daniel Persson"));
---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,10 @@
+
+const getFirstName = (fullName) => {
+    return fullName.split(" ")[0]
+}
+console.log(getFirstName("Daniel Persson"))
+
+
+const getLastName = (fullName) => fullName.split(" ")[1]
+console.log(getLastName("Daniel Persson"))
+
